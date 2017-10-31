ec4d757a411cb862270c0b7f096dad38924cb890
ahmer munaf <ahmermunaf@gmail.com>
2017-10-31 13:41:19 +0500

    working!

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Navbar , FormControl , FormGroup , Col , Checkbox , Button } from 'react-bootstrap'
+import { Navbar , FormControl , Jumbotron , FormGroup , Col , Checkbox , Button } from 'react-bootstrap'
 
 class App extends Component {
   constructor(){
@@ -31,13 +31,43 @@ class App extends Component {
           </Navbar>
         </div>
         <div>
+          <Jumbotron style={{boxShadow:'1px 1px 5px grey',backgroundColor:'#fcfcfc',marginLeft:'15%',width:'70%'}}>
           <span>
-            <Checkbox checked={this.state.allFlag} onClick={()=>{
+            <Checkbox style={{marginLeft:15,marginTop:10}} checked={this.state.allFlag} onClick={()=>{
               this.setState({allFlag:!this.state.allFlag})
             }}>
               Select All
             </Checkbox>
           </span>
+          <div>
+            <ul>
+              <li style={{borderBottomWidth:0.5,borderBottomColor:'#ededed',borderBottomStyle:'solid',paddingTop:10,paddingBottom:10}}>
+                <span>
+                  popup 1
+                </span>
+                <span style={{marginLeft:'80%'}}>
+                  <Checkbox style={{display:'inline'}}>select</Checkbox>
+                </span>
+              </li>
+              <li style={{borderBottomWidth:0.5,borderBottomColor:'#ededed',borderBottomStyle:'solid',paddingTop:10,paddingBottom:10}}>
+              <span>
+                popup 2
+              </span>
+              <span style={{marginLeft:'80%'}}>
+                <Checkbox style={{display:'inline'}}>select</Checkbox>
+              </span>
+              </li>
+              <li style={{borderBottomWidth:0.5,borderBottomColor:'#ededed',borderBottomStyle:'solid',paddingTop:10,paddingBottom:10}}>
+                <span>
+                  popup 3
+                </span>
+                <span style={{marginLeft:'80%'}}>
+                  <Checkbox style={{display:'inline'}}>select</Checkbox>
+                </span>
+                </li>
+            </ul>
+          </div>
+          </Jumbotron>
         </div>
       </div>
     );
